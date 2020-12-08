c75ed0e68bdec47ccfeb3ea15bcaca02af3552e4
Lucio Mestrinare <lucio@flysys.com.br>
2020-12-08 10:52:32 -0300

    [fix] package path

--- Autentique.Net/Properties/AssemblyInfo.cs
+++ Autentique.Net/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Definir ComVisible como false torna os tipos neste assembly invisíveis
 // para componentes COM. Caso precise acessar um tipo neste assembly de
 // COM, defina o atributo ComVisible como true nesse tipo.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
 [assembly: Guid("1b014d3f-8b06-402a-9eb9-dc8af5418252")]
